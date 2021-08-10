b15efa26dfc901a00d1ab63708708bfacc85c8df
Stefan Galler <galler.stefan@gmail.com>
2021-08-10 09:44:34 +0200

    cleanup main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:theming/global_providers.dart';
 
 void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await initGlobalProviders();
   runApp(ProviderScope(
     child: MyApp(),
   ));
--- lib/theme.dart
+++ lib/theme.dart
@@ -1,12 +1,59 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 // THEME PROVIDERS
+final theme = Provider((ref) => _theme);
+final darkTheme = Provider((ref) => _darkTheme);
 
 // THEMES
 // light
-final _theme = ThemeData();
+final _theme = ThemeData(
+  primaryColor: Colors.deepPurple,
+  elevatedButtonTheme: ElevatedButtonThemeData(
+    style: ElevatedButton.styleFrom(
+      primary: Colors.deepPurple,
+      shape: StadiumBorder(),
+    ),
+  ),
+  textTheme: TextTheme(
+      headline1: TextStyle(
+        fontSize: 48,
+        fontWeight: FontWeight.bold,
+        color: Colors.black87,
+      ),
+      headline2: TextStyle(
+        fontSize: 36,
+        fontWeight: FontWeight.bold,
+        color: Colors.black54,
+      )),
+);
+
+final _customTheme = CustomThemeData(
+  imageSize: 150,
+);
 
 //dark
-final _darkTheme = ThemeData();
+final _darkTheme = ThemeData(
+  brightness: Brightness.dark,
+  primaryColor: Colors.deepPurple,
+  elevatedButtonTheme: _theme.elevatedButtonTheme,
+  textTheme: _theme.textTheme.apply(
+    displayColor: Colors.white,
+    bodyColor: Colors.white,
+  ),
+);
+
+final _customDarkTheme = CustomThemeData();
 
 // EXTENSIONS AND CLASSES
+extension CustomTheme on ThemeData {
+  CustomThemeData get custom => brightness == Brightness.dark ? _customDarkTheme : _customTheme;
+}
+
+class CustomThemeData {
+  final double imageSize;
+
+  CustomThemeData({
+    this.imageSize = 100,
+  });
+}
