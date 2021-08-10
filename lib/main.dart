17b6c2c2a064c0f0ed0abb68df30e7218bf9dee5
Stefan Galler <galler.stefan@gmail.com>
2021-08-10 09:45:29 +0200

    cleanup main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 void main() async {
-  runApp(ProviderScope(
-    child: MyApp(),
-  ));
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
